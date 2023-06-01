0a0379f8abe49fe33592ddb313320a2a9ecc9be9
hdzylovesyou <106484628+hdzy@users.noreply.github.com>
2023-06-01 21:15:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let currentState = '';
+let activeCircle = document.querySelector('.circle-active');
+let activeLCircle, activeText;
+
+
+document.querySelectorAll('#circle-client,' +
+    ' #circle-med,' +
+    ' #circle-phase,' +
+    ' #circle-medotd,' +
+    ' #circle-reg, ' +
+    ' #circle-l-client,' +
+    ' #circle-l-med,' +
+    ' #circle-l-phase,' +
+    ' #circle-l-medotd,' +
+    ' #circle-l-reg,' +
+    ' #info-client,' +
+    ' #info-med,' +
+    ' #info-phase,' +
+    ' #info-medotd,' +
+    ' #info-reg').forEach(e => {
+        e.addEventListener('click', changeCircleState, e.target)
+})
+
+function changeCircleState(el) {
+    let elementType;
+
+    if (el.target.dataset.section !== undefined) {
+        elementType = el.target.dataset.section;
+
+    } else elementType = el.target.parentNode.dataset.section;
+
+    currentState = elementType;
+
+    if (activeCircle != null) {
+        activeCircle.classList.remove('circle-active');
+    }
+
+    activeCircle = document.getElementById(`circle-${elementType}`);
+    activeCircle.classList.add('circle-active');
+
+    if (activeLCircle != null) {
+        activeLCircle.classList.remove('circle-l-active');
+    }
+
+    activeLCircle = document.getElementById(`circle-l-${elementType}`);
+    activeLCircle.classList.add('circle-l-active');
+
+    if (activeText != null) {
+        activeText.classList.remove('text-active');
+    }
+
+    activeText = document.getElementById(`info-${elementType}`);
+    activeText.classList.add('text-active');
+
+}
